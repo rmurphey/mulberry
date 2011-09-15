f93060dfcfbd9a246ccf15ed90fedbda5f2a5c54
Rebecca Murphey <rmurphey@gmail.com>
2011-09-15 19:26:49 -0400

    only show error message on one form at a time

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 (function($){
 
 $(function() {
-  $('form.signup-form').submit(function() {
+  var $forms = $('form.signup-form').submit(function() {
+
     var n = $('#entry_0').val(),
         e = $('#entry_1').val(),
         $this = $(this),
@@ -10,18 +11,20 @@ $(function() {
     $('.error').empty().removeClass('error');
 
     if ($.trim(n) && $.trim(e)) {
+      // there was input in both fields
       $this.fadeTo(100, 0.5);
 
-      $this.replaceWith('<p class="thanks">Thanks for signing up -- we&rsquo;ll be in touch!</p>');
+      $forms.replaceWith('<p class="thanks">Thanks for signing up -- we&rsquo;ll be in touch!</p>');
       $('.error-msg .inner, .extra-info').fadeOut();
       return true;
     }
 
-    $('.error-msg .inner')
+    $signup.siblings('.error-msg').find('.inner')
       .text('Please fill out both fields')
       .fadeIn();
 
     return false;
+
   });
 
 });
