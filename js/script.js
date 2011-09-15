5f9946069572d6ee011e12b584f4c4096ecaf864
Dan Imal <dimal@toura.com>
2011-09-15 13:16:19 -0400

    improve form info styles

--- js/script.js
+++ js/script.js
@@ -11,16 +11,13 @@ $(function() {
     if ($.trim(n) && $.trim(e)) {
       $this.fadeTo(100, 0.5);
 
-      setTimeout(function(){
-        $this.slideUp(100, function() {
-          $this.replaceWith('<p class="thanks">Thanks for signing up -- we&rsquo;ll be in touch!</p>');
-        });
-      }, 500);
+      $this.replaceWith('<p class="thanks">Thanks for signing up -- we&rsquo;ll be in touch!</p>');
 
       return true;
+      return false;
     }
 
-    $this.before('<p class="signup-error">Please fill out both fields</p>');
+    $('#signup .extra-info').text('Please fill out both fields');
     return false;
   });
 
