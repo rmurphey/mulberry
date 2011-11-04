8689433e652f2340e8c3d7a06fe9611bdf77a10c
Matt Henry <greenideas@gmail.com>
2011-11-04 17:35:34 -0400

    test new style featured images

--- toura_app/spec/javascripts/model_featured_image_spec.js
+++ toura_app/spec/javascripts/model_featured_image_spec.js
@@ -1,16 +1,31 @@
 describe("featured image model", function() {
-  var api, imageObj;
+  var api, imageObj, newImageObj;
 
   beforeEach(function() {
     dojo.require('toura.models.FeaturedImage');
 
     api = dataAPI;
+
     imageObj = api.getById('image-540');
+
+    newImageObj = {
+      "image": {
+        "_reference": "image-540"
+      }
+    };
   });
 
-  it("should create a featured image model", function() {
+  it("should create a featured image model from old style data", function() {
     var model = new toura.models.FeaturedImage(api._store, imageObj);
     expect(model.large).toBeDefined();
+    expect(model.small).toBeDefined();
+    expect(model.large.url).toBeDefined();
+    expect(model.small.url).toBeDefined();
+  });
+
+  it("should create a featured image model from new style data", function() {
+    var model = new toura.models.FeaturedImage(api._store, newImageObj);
+    expect(model).toBeDefined();
     expect(model.small).toBeDefined();
     expect(model.large.url).toBeDefined();
     expect(model.small.url).toBeDefined();
