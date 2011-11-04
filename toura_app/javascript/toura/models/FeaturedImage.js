46c5bee62b0bd07abd77c0eac1dc72ccd218782f
Matt Henry <greenideas@gmail.com>
2011-11-04 12:07:51 -0400

    account for new JSON structure

--- toura_app/javascript/toura/models/FeaturedImage.js
+++ toura_app/javascript/toura/models/FeaturedImage.js
@@ -4,7 +4,13 @@ dojo.require('toura.models._StorableAsset');
 
 dojo.declare('toura.models.FeaturedImage', [ toura.models._StorableAsset ], {
   constructor :  function(store, item) {
-    this._getUrl(store, item);
+    store.fetchItemByIdentity({
+      identity : item._reference || item.image._reference,
+      onItem : function(item) {
+        this._getUrl(store, item);
+      },
+      scope : this
+    });
     this.small = this.featuredSmall;
     this.large = this.featured;
     delete this.featured_small;
