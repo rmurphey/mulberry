5d9d6402bd501379dbc59dc929a40db3a43de001
Matt Henry <greenideas@gmail.com>
2011-11-07 10:05:08 -0500

    add spec for grid view

---
+++ spec/integration/grid_view_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+include CapybaraSpecHelper
+
+describe "Grid View", :type => :request do
+  it "should have child node links" do
+    DEVICES.each do |d|
+      visit "/#{d[:os]}/#{d[:type]}/#/node/node-grid"
+
+      [:grid_child_one, :grid_child_two].each do |child_link|
+        page.should have_css "li a[href$=#{child_link}]"
+      end
+
+      click_link "Grid Child One"
+      page.should have_css ".body-text"
+    end
+  end
+end
