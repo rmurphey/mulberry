59c9ea712b06e5c020ee92382167cf9fdc73c1fb
Daniel Kim <danieldkimster@gmail.com>
2011-11-09 14:55:11 -0500

    Handle Videos1 template mapping in PageFactory [#20736893]

--- toura_app/javascript/toura/app/PageFactory.js
+++ toura_app/javascript/toura/app/PageFactory.js
@@ -19,8 +19,10 @@ dojo.declare('toura.app.PageFactory', [], {
   },
 
   _translations : {
-    'videos'            : 'Videos1',
-    'locations-map'     : 'GoogleMap1'
+    'videos'                 : 'Videos1',
+    'videos-and-text-phone'  : 'Videos1',
+    'videos-and-text-tablet' : 'Videos1',
+    'locations-map'          : 'GoogleMap1'
   },
 
   /**
