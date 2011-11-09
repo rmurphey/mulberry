130cfb7d75e085ede16b2da58c3db1a3152da52b
Rebecca Murphey <rmurphey@gmail.com>
2011-11-09 13:46:07 -0500

    better custom route test coverage for twitter demo

--- spec/integration/twitter/smoke_spec.rb
+++ spec/integration/twitter/smoke_spec.rb
@@ -3,8 +3,16 @@ require 'integration/twitter/spec_helper'
 include CapybaraSpecHelper
 
 describe "Twitter demo smoke test", :type => :request do
-  it "should boot the twitter demo app" do
-    visit '/'
-    page.should have_css '.component.latest-tweet'
+  DEVICES.each do |d|
+    it "should boot the twitter demo app" do
+      visit "/#{d[:os]}/#{d[:type]}/#/home"
+      page.should have_css '.component.latest-tweet'
+    end
+
+    it "should display the dynamic page" do
+      visit "/#{d[:os]}/#{d[:type]}/#/twitter/touradev"
+      page.should have_content 'touradev'
+      page.should have_css '.component.tweets'
+    end
   end
 end
