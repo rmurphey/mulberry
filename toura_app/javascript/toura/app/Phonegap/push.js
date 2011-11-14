7ee5f372faed8b24aca41c0b051f8443dc4bdd50
Rebecca Murphey <rmurphey@gmail.com>
2011-11-14 23:54:28 +0000

    trying to get phonegap dir named correctly

--- toura_app/javascript/toura/app/Phonegap/push.js
+++
@@ -1,8 +0,0 @@
-dojo.provide('toura.app.PhoneGap.push');
-
-dojo.require('vendor.urbanairship.push');
-
-toura.app.PhoneGap.push = function(pg, device) {
-  if (!pg) { return; }
-  vendor.urbanairship.push();
-};
