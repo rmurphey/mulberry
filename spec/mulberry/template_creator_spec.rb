7b10d7b153b15d446f9a866a729697b9cc54c486
Rebecca Murphey <rmurphey@gmail.com>
2011-11-15 23:35:41 +0000

    basic test for template creation

--- spec/mulberry/template_creator_spec.rb
+++ spec/mulberry/template_creator_spec.rb
@@ -3,6 +3,7 @@ require 'mulberry/template_creator'
 
 describe Mulberry::TemplateCreator do
   before :each do
+    @template_dir = 'templates'
     @tmpdir = Dir.mktmpdir
   end
 
@@ -13,9 +14,24 @@ describe Mulberry::TemplateCreator do
   it "should create a template with the requested name" do
     Mulberry::TemplateCreator.new('template', @tmpdir, 'foo')
 
-    template_dir = File.join @tmpdir, 'templates'
+    template_dir = File.join @tmpdir, @template_dir
 
     File.exists?(template_dir).should be_true
     Dir.entries(template_dir).include?('foo.yml').should be_true
+  end
+
+  it "should create a template" do
+    tpl = 'foo'
+
+    Mulberry::TemplateCreator.new('template', @tmpdir, tpl)
+
+    template_file = File.join(@tmpdir, @template_dir, "#{tpl}.yml")
+    template = YAML.load_file(template_file)[tpl]
+
+    template['screens'].should_not be_nil
+    template['screens'].length.should be > 0
+    template['screens'].first['name'].should_not be_nil
+    template['screens'].first['regions'].should_not be_nil
+    template['screens'].first['regions'].length.should be > 0
   end
 end
