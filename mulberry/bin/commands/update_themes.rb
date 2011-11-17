4af557d821a36fdfde9e534e59a46285d5fc3e3e
Dan Imal <dimal@toura.com>
2011-11-17 11:37:04 -0500

    change to update_themes

--- mulberry/bin/commands/update_themes.rb
+++ mulberry/bin/commands/update_themes.rb
@@ -1,6 +1,6 @@
 module Mulberry
   module Command
-    class Update
+    class UpdateThemes
       def initialize(args)
         to_dir = Mulberry.get_app_dir
         Mulberry::App.update_themes to_dir
