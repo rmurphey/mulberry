9be969a996bdb6e7228cd7de9a8e0a5e92b7eae8
Rebecca Murphey <rmurphey@gmail.com>
2011-11-28 16:49:27 -0500

    version bumps

--- mulberry/mulberry.rb
+++ mulberry/mulberry.rb
@@ -20,7 +20,7 @@ module Mulberry
   class ConfigError < RuntimeError
   end
 
-  VERSION     = '0.1.1'
+  VERSION     = '0.2'
   CONFIG      = 'config.yml'
   CONFIG_DEV  = 'config_dev.yml'
   SITEMAP     = 'sitemap.yml'
