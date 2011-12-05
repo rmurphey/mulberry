84320d3c6738dd719656761b1a05133cd3dd20f5
Matt Rogish <matt.rogish@gmail.com>
2011-12-05 18:31:42 -0500

    Removing touch

--- mulberry/server.rb
+++ mulberry/server.rb
@@ -19,12 +19,6 @@ module Mulberry
       @js_build_name = 'dev'
     end
 
-    def send_file(path, opts={})
-      # make sure it never caches
-      FileUtils.touch(path) if File.exists? path
-      super(path, opts={})
-    end
-
     private
 
     #####################
