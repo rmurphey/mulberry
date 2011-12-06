bd77530518dd2fb2e2b86b1f528fedbe2582fced
Rebecca Murphey <rmurphey@gmail.com>
2011-12-06 15:59:37 -0500

    update twitter demo routes

--- demos/twitter/javascript/base.js
+++ demos/twitter/javascript/base.js
@@ -1,3 +1,4 @@
+dojo.require('client.routes');
 dojo.require('client.components.LatestTweet');
 dojo.require('client.components.UserList');
 dojo.require('client.components.UserInfo');
@@ -5,13 +6,3 @@ dojo.require('client.components.Tweets');
 dojo.require('client.capabilities.Twitter');
 dojo.require('client.stores.Twitter');
 
-mulberry.route('/twitter/:username', function(params) {
-  var twitter = new client.stores.Twitter(),
-      page = mulberry.app.PageFactory.createPage({
-        pageController : 'user',
-        tweets : twitter.getAll(params.username),
-        name : params.username
-      });
-
-  mulberry.app.UI.showPage(page);
-});
