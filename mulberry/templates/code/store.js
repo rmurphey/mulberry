520bf36f41da1509d9f781353d54915d4e36c5c7
Rebecca Murphey <rmurphey@gmail.com>
2011-12-05 21:24:29 -0500

    update inline docs about created store

--- mulberry/templates/code/store.js
+++ mulberry/templates/code/store.js
@@ -2,23 +2,9 @@ dojo.provide('client.stores.{{name}}');
 
 mulberry.stores.local('{{name}}', {
   /*
-   * You can create an instance of this store by doing:
+   * This creates a store at client.stores.{{name}}
    *
-   *    var myInstance = new client.stores.{{name}}();
-   *
-   * You may also wish to uncomment the code at the bottom of this file,
-   * which will automatically create an instance of this store when your
-   * application loads. The created instance will be available to the rest of
-   * your code as client.stores.{{name}}, and the original constructor will no
-   * longer exist.
+   * You can augment this store by adding properties and methods to this
+   * object.
    */
 });
-
-/*
-(function() {
-var s = dojo.subscribe('/app/started', function() {
-  client.stores.{{name}} = new client.stores.{{name}}();
-  dojo.unsubscribe(s);
-});
-}());
-*/
