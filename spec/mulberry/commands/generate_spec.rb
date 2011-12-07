0f2b2a521750037ef7fffcd54fb55ff970a48210
Rebecca Murphey <rmurphey@gmail.com>
2011-12-07 13:00:39 -0500

    generate nonexistent custom components that are included in user templates

--- spec/mulberry/commands/generate_spec.rb
+++ spec/mulberry/commands/generate_spec.rb
@@ -2,9 +2,12 @@ require 'mulberry/commands/spec_helper.rb'
 
 describe Mulberry::Command::Generate do
   include Mulberry::Command::SpecHelpers
-  before do
+  before :each do
     @app_name = 'fooapp'
+    FileUtils.rm_rf @app_name
+  end
 
+  after :each do
     Dir.chdir Mulberry::Directories.root
     FileUtils.rm_rf @app_name
   end
@@ -41,5 +44,39 @@ describe Mulberry::Command::Generate do
     Mulberry::Command::Generate.new(["#{Dir.pwd}/#{@app_name}"])
     File.exists?(File.join(@app_name, 'pages', 'fake_page.md')).should be_true
   end
-end
 
+  describe "component creation" do
+    before :each do
+      Mulberry::Command::Scaffold.new([@app_name])
+      FileUtils.cp_r(
+        File.join(FIXTURES_DIR, 'template.yml'),
+        File.join(@app_name, 'templates')
+      )
+
+      FileUtils.touch File.join(@app_name, 'javascript', 'components', 'AlreadyExists.js')
+
+      Dir.chdir @app_name
+      Mulberry::Command::Generate.new
+    end
+
+    it "should create nonexistent components" do
+      created_component = File.join('javascript', 'components', 'NonexistentComponent.js')
+
+      File.exists?(created_component).should be_true
+      File.read(created_component).should include 'mulberry.component'
+      File.read(created_component).should include 'NonexistentComponent'
+
+    end
+
+    it "should not re-create an already existing component" do
+      existing = File.join('javascript', 'components', 'AlreadyExists.js')
+      File.exists?(existing).should be_true
+      File.read(existing).should_not include 'mulberry.component'
+    end
+
+    it "should not create non-custom components" do
+      File.exists?(File.join('javascript', 'components', 'BuiltInComponent.js')).should_not be_true
+    end
+  end
+
+end
