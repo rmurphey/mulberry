8198838260d6497456e2ad7a896bed90d2a215d7
Rebecca Murphey <rmurphey@gmail.com>
2011-12-07 13:30:17 -0500

    better test for template names

--- spec/mulberry/template_creator_spec.rb
+++ spec/mulberry/template_creator_spec.rb
@@ -21,7 +21,7 @@ describe Mulberry::TemplateCreator do
   end
 
   it "should create a template" do
-    tpl = 'foo'
+    tpl = 'foo-bar'
 
     Mulberry::TemplateCreator.new('template', @tmpdir, tpl)
 
@@ -34,19 +34,19 @@ describe Mulberry::TemplateCreator do
     template['screens'].first['regions'].should_not be_nil
     template['screens'].first['regions'].length.should be > 0
     template['capabilities'].should_not be_nil
-    template['capabilities'].first['name'].should == 'PageFoo'
+    template['capabilities'].first['name'].should == 'PageFooBar'
   end
 
   it "should create a capability for the page" do
-    tpl = 'foo'
+    tpl = 'foo-bar'
 
     Mulberry::TemplateCreator.new('template', @tmpdir, tpl)
 
-    capability_file = File.join(@tmpdir, 'javascript', 'capabilities', 'PageFoo.js')
+    capability_file = File.join(@tmpdir, 'javascript', 'capabilities', 'PageFooBar.js')
 
     File.exists?(capability_file).should be_true
     File.read(capability_file).should include 'mulberry.capability'
-    File.read(capability_file).should include 'PageFoo'
+    File.read(capability_file).should include 'PageFooBar'
   end
 
 end
