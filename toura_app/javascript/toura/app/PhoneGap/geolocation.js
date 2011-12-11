57f9b9f5309b46d4c605fba86c8b9d68b502fff1
Rebecca Murphey <rmurphey@gmail.com>
2011-12-11 11:58:42 -0500

    geolocation api

---
+++ toura_app/javascript/toura/app/PhoneGap/geolocation.js
@@ -0,0 +1,61 @@
+dojo.provide('toura.app.PhoneGap.geolocation');
+
+toura.app.PhoneGap.registerAPI('geolocation', function(pg, device) {
+  var noop = function(){};
+
+  return {
+    getCurrentPosition : function(success, error, opts) {
+      var dfd = new dojo.Deferred(),
+
+          win = function(data) {
+            (success || noop)(data);
+            dfd.resolve(data);
+          },
+
+          fail = function(msg) {
+            (error || noop)(msg);
+            dfd.reject(msg);
+          },
+
+          failMsg = 'Geolocation API not available';
+
+      if (navigator.geolocation && navigator.geolocation.getCurrentPosition) {
+        navigator.geolocation.getCurrentPosition(win, fail, opts);
+      } else {
+        fail(msg);
+      }
+
+      return dfd.promise;
+    },
+
+    watchPosition : function(success, error, opts) {
+      var dfd = new dojo.Deferred(),
+
+          win = function(data) {
+            (success || noop)(data);
+            dfd.resolve(data);
+          },
+
+          fail = function(msg) {
+            (error || noop)(msg);
+            dfd.reject(msg);
+          },
+
+          watchId;
+
+      if (navigator.geolocation && navigator.geolocation.watchPosition) {
+        dfd.promise.watchId = navigator.geolocation.watchPosition(win, fail, opts);
+      } else {
+        dfd.promise.watchId = true;
+      }
+    },
+
+    clearWatch : function(watch) {
+      var watchId = watch.watchId || watch;
+
+      if (navigator.geoLocation && navigator.geolocation.clearWatch && watchId) {
+        navigator.geolocation.clearWatch(watchId);
+      }
+    }
+  };
+});
