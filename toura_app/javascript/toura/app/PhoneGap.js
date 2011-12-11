9eeac7a3d1116c24ad619226906212dd5dec4542
Rebecca Murphey <rmurphey@gmail.com>
2011-12-11 12:43:04 -0500

    move registerAPI to its own file

--- toura_app/javascript/toura/app/PhoneGap.js
+++ toura_app/javascript/toura/app/PhoneGap.js
@@ -1,5 +1,6 @@
 dojo.provide('toura.app.PhoneGap');
 
+dojo.require('toura.app.PhoneGap._base');
 dojo.require('toura.app.PhoneGap.notification');
 dojo.require('toura.app.PhoneGap.device');
 dojo.require('toura.app.PhoneGap.network');
@@ -10,12 +11,3 @@ dojo.require('toura.app.PhoneGap.browser');
 dojo.require('toura.app.PhoneGap.camera');
 dojo.require('toura.app.PhoneGap.geolocation');
 
-toura.app.PhoneGap.registerAPI = function(name, module) {
-  var s = dojo.subscribe('/app/deviceready', function() {
-    var device = toura.app.Config.get('device'),
-        phonegapPresent = toura.app.PhoneGap.present = window.device && window.device.phonegap;
-
-    toura.app.PhoneGap[name] = module(phonegapPresent, device);
-    dojo.unsubscribe(s);
-  });
-};
