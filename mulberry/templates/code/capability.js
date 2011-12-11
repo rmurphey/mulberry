ac3514a1e667d2274fdb8832bdd40e9b2fa61836
Dan Imal <dimal@toura.com>
2011-12-11 15:18:35 -0500

    helpful comments

--- mulberry/templates/code/capability.js
+++ mulberry/templates/code/capability.js
@@ -2,9 +2,17 @@ dojo.provide('client.capabilities.{{name}}');
 
 mulberry.capability('{{name}}', {
   requirements : {
+  /*
+   *  <componentLocalVariableName> : '<componentName>'
+   *  e.g.:
+   *  myImageThingee : 'ImageThingee'
+   */
   },
 
   connects : [
+  /*
+   * '<componentLocalVariableName>', '<eventName>', '<methodName>'
+   */
   ],
 
   init : function() {
