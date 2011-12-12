9faafaab90288b2a7b9e86f26eb01bad561893e2
Rebecca Murphey <rmurphey@gmail.com>
2011-12-12 16:54:53 -0500

    removing specs for moved demo

--- spec/integration/phonegap_apis/smoke_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'integration/phonegap_apis/spec_helper'
-
-include CapybaraSpecHelper
-
-describe "PhoneGap APIs Smoke Test", :type => :request do
-  it_should_behave_like "all demo apps"
-end
