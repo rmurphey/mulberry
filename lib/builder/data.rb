6f39a579db64c6bf899b5197465884c6ea080e7d
Daniel Kim <danieldkimster@gmail.com>
2011-12-14 16:46:15 -0500

    Fix publish of ota on deploy [#20095099]

--- lib/builder/data.rb
+++ lib/builder/data.rb
@@ -10,10 +10,14 @@ module Builder
       json = JSON.pretty_generate(@build.build_helper.data)
       if @build.ota_enabled?
         toura_api_config = @build.settings[:toura_api_config]
-        ota_service_application = OtaServiceApplication.new(toura_api_config.url,
-                                                            toura_api_config.key,
-                                                            toura_api_config.secret)
-        ota_service_application.publish json unless ota_service_application.version
+        ota_service_application = OtaServiceApplication.new(toura_api_config['url'],
+                                                            toura_api_config['key'],
+                                                            toura_api_config['secret'])
+        begin
+          ota_service_application.version
+        rescue Mulberry::Http::NotFound
+          ota_service_application.publish json
+        end
       end
       File.open(@destination, 'w') do |f|
         f.write "toura.data.local = #{json};"
