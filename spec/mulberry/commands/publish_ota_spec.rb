5051116e65b4096d435d3b1cc1bc747207276514
Daniel Kim <danieldkimster@gmail.com>
2011-12-15 16:19:03 -0500

    Mulberry::Command::PublishOta spec needs config set up with ota [#20095099]

--- spec/mulberry/commands/publish_ota_spec.rb
+++ spec/mulberry/commands/publish_ota_spec.rb
@@ -11,7 +11,19 @@ describe Mulberry::Command::PublishOta do
     end
 
     it "should print error messages in a friendly manner" do
+      config_path = File.join(Dir.pwd, @app.name, "config.yml")
+      config = YAML::load( File.open(config_path) )
+      config['ota'] = { 'enabled' => true }
+      config['toura_api'] = {
+        'url' => 'https://myapi.com',
+        'key' => 'some_key'
+      }
+      File.open(config_path, 'w') { |f| f.write(config.to_yaml) }
+
       ["404", "503"].each do |status|
+        [:get, :post].each do |method|
+          FakeWeb.register_uri(method, //, :status => status)
+        end
         FakeWeb.register_uri(:post, //, :status => status)
         stdout = capture_io_streams(:stdout) do
           lambda { Mulberry::Command::PublishOta.new([@app.name]) }.should_not raise_error
