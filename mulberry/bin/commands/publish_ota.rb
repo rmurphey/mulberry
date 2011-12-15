bf4b1ed2d3213fe9eed71fefdb3ef7a6967e472e
Daniel Kim <danieldkimster@gmail.com>
2011-12-15 15:33:22 -0500

    fix argument handling in Mulberry::Command::PublishOta [#20095099]

--- mulberry/bin/commands/publish_ota.rb
+++ mulberry/bin/commands/publish_ota.rb
@@ -3,22 +3,22 @@ module Mulberry
     class PublishOta
       def initialize(args)
         data_json = nil
-        command = "publish_ota"
-        args = []
+        app_method = "publish_ota"
+        app_method_args = []
         OptionParser.new do |opts|
           opts.banner = "Usage: mulberry publish_ota [project_directory] [options]"
           opts.on("-f FILE", "--file FILE", String, "Location of tour json file.") do |p|
-            args << File.read(p)
+            app_method_args << File.read(p)
           end
 
           opts.on("--current", "Show currently published version.") do
-            command = "ota_version"
+            app_method = "ota_version"
           end
         end.parse!
 
         dir = Mulberry.get_app_dir args[0]
         begin
-          app = Mulberry::App.new(dir).send(command, *args)
+          app = Mulberry::App.new(dir).send(app_method, *app_method_args)
         rescue Mulberry::Http::Exception
           puts "Error: #{$!}"
         end
