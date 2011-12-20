015fac50f5109fd306c78f9000ec6512be8eefdb
Rebecca Murphey <rmurphey@gmail.com>
2011-12-20 16:54:30 -0500

    first pass at checking for components

--- spec/integration/demo_spec_helper.rb
+++ spec/integration/demo_spec_helper.rb
@@ -1,26 +1,45 @@
 require 'spec_helper'
+require 'active_support/inflector'
 
 shared_examples_for "all demo apps" do
   describe "Demo app page smoke test", :type => :request do
 
     $app.should_not == nil
     $templates.should_not == nil
-    p $templates
+
+    # these are components that remove themselves if there is no data for them
+    # TODO: we should be smarter about this
+    @ignored_components = [ 'PageHeaderImage', 'ChildNodes' ]
 
     DEVICES.each do |d|
       $app.data[:items].each do |item|
 
         if item[:type] == 'node'
-          node_page_controller = item[:pageController][d[:type]]
+          node_page_controller = item[:pageController][d[:type]] || 'default'
 
           if node_page_controller != 'locations-map'
+            components = []
             config = $templates[node_page_controller.to_s]
-            # p config
+
+            raise "No config for #{node_page_controller}" unless config
+
+            config['screens'].each do |s|
+              s['regions'].each do |r|
+                r['components'].each do |c|
+                  components << c.gsub!('custom.', '') unless @ignored_components.map { |str| c.match(str) }.length
+                end unless !r['components']
+              end
+            end
 
             it "should render node #{item[:id]} for #{d[:os]}/#{d[:type]}" do
               visit "/#{d[:os]}/#{d[:type]}/#/node/#{item[:id]}"
 
               page.should have_css ".page-#{item[:id]}"
+
+              components.each do |c|
+                puts "checking for #{c}"
+                page.should have_css ".component.#{c.underscore.dasherize.downcase}"
+              end
             end
           else
             puts "SKIPPING NODE #{item[:id]} - pageController = 'locations-map'"
