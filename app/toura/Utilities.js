b41cd6018bb04a335dec724155b431cf0c31a5a8
Rebecca Murphey <rmurphey@gmail.com>
2012-01-04 14:06:08 -0500

    just delegate stuff to the config object, but find a callback param name if present

--- app/toura/Utilities.js
+++ app/toura/Utilities.js
@@ -58,15 +58,10 @@ toura.haml = Haml;
 
 toura.jsonp = function(url, config) {
   config = dojo.isObject(url) ? url : (config || {});
+  config.callbackParamName = config.callback || config.callbackParamName || 'callback';
   url = dojo.isString(url) ? url : config.url;
 
   if (!url) { return; }
 
-  return dojo.io.script.get({
-    url : url,
-    handleAs : 'json',
-    callbackParamName : config.callback || 'callback',
-    load : config.load,
-    error : config.error
-  });
+  return dojo.io.script.get(dojo.delegate(config, { url : url }));
 };
