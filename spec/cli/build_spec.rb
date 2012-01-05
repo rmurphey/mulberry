71336ea50046f7b02519a976c6cbff253645ced2
Rebecca Murphey <rmurphey@gmail.com>
2012-01-05 09:35:28 -0500

    one more test

--- spec/cli/build_spec.rb
+++ spec/cli/build_spec.rb
@@ -210,6 +210,7 @@ describe Builder::Build do
     it "should properly populate the manifest" do
       manifest = File.read(File.join(@bundle[:location], 'android', 'AndroidManifest.xml'))
       manifest.should include 'android:versionName="1.0"'
+      manifest.should include 'com.toura.app2_fake'
     end
 
     after do
