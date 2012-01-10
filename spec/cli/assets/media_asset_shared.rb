133c71445053e71176c0c641ad5fb08659d946e4
Matt Rogish <matt.rogish@gmail.com>
2012-01-09 19:28:05 -0500

    Removing junk comment

--- spec/cli/assets/media_asset_shared.rb
+++ spec/cli/assets/media_asset_shared.rb
@@ -1,8 +1,6 @@
 shared_examples_for "all media assets" do
   describe '#item', :fakefs => true  do
     it "should handle output remote items properly" do
-      #FakeFS::FileSystem.clone @remote_asset.asset_file if @remote_asset.is_a? Mulberry::Asset::Image
-
       item = @remote_asset.item
 
       item[:url].should match /^http/
