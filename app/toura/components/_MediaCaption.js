5e8ab021094ae5fe7ffec18544249abbe8f231a4
Evan Hensleigh <ehensleigh@toura.com>
2012-01-19 16:41:51 -0500

    Style fixes for MediaCaption.

--- app/toura/components/_MediaCaption.js
+++ app/toura/components/_MediaCaption.js
@@ -7,21 +7,18 @@ dojo.declare('toura.components._MediaCaption', toura.components.BodyText, {
   _fixCaptionAlignment: function() {
     // by default, the domNode should span the full width, as it's a block
     // element
-    
-    // this value could be cached, but we'd then need separate functions for
-    // /content/update and /window/resize
-    var fullwidth = dojo.position(this.domNode).w,
-        textwidth;
+    var fullWidth = dojo.position(this.domNode).w,
+        textWidth;
 
     // we add float:left so this will carry a width determined by its content
     // rather than the window
     dojo.style(this.domNode, { 'float' : 'left' });
     
-    textwidth = dojo.position(this.domNode).w;
-    if (fullwidth > textwidth) {
-      dojo.addClass(this.domNode, 'caption-center');
+    textWidth = dojo.position(this.domNode).w;
+    if (fullWidth > textWidth) {
+      this.addClass('caption-center');
     } else {
-      dojo.removeClass(this.domNode, 'caption-center');
+      this.removeClass('caption-center');
     }
     
     // remove the float, since it's no longer needed
