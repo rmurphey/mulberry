074fec0e6700dff2ccc29947e1ead75c01ac990c
Matt Rogish <matt.rogish@gmail.com>
2012-01-30 12:16:05 -0500

    New RVM version, new install cmd

--- install/ubuntu/install.sh
+++ install/ubuntu/install.sh
@@ -1,7 +1,8 @@
 sudo apt-get update
 sudo apt-get install build-essential git git-core curl zlib1g zlib1g-dev libxml2-dev libxslt-dev openjdk-6-jdk xvfb unzip
 
-bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
 rvm pkg install zlib
 rvm pkg install openssl
