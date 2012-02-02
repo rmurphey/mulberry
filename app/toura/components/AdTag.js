2eeee2ad65340ed14f21c12613347a8c31bbfe70
Matt Henry <greenideas@gmail.com>
2012-02-02 18:50:00 -0500

    add basic ad tag component

---
+++ app/toura/components/AdTag.js
@@ -0,0 +1,15 @@
+dojo.provide('toura.components.AdTag');
+
+dojo.require('toura._Component');
+
+dojo.declare('toura.components.AdTag', toura._Component, {
+  templateString : dojo.cache('toura.components', 'AdTag/AdTag.haml'),
+
+  prep : function() {
+
+  },
+
+  init : function() {
+
+  }
+});
