546b4878323dca17d179b277056ee0aacc7aa8f4
Matt Henry <greenideas@gmail.com>
2012-02-07 14:19:03 -0500

    add src attribute to ad iframe

--- app/toura/components/AdTag.js
+++ app/toura/components/AdTag.js
@@ -5,11 +5,14 @@ dojo.require('toura._Component');
 dojo.declare('toura.components.AdTag', toura._Component, {
   templateString : dojo.cache('toura.components', 'AdTag/AdTag.haml'),
 
-  prep : function() {
+  adjustMarkup : function() {
+    var src = dojo.string.substitute("http://${host}/${os}/${device}/media/html/ad-${device}.html",
+      {
+        host   : window.location.host,
+        os     : this.device.os,
+        device : this.device.type
+      });
 
-  },
-
-  init : function() {
-
+    dojo.attr(this.adFrame,"src",src);
   }
 });
