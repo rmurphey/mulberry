40ba1bea503a401b2d57e4a8b5453c641f1a6ee2
Matt Henry <greenideas@gmail.com>
2012-02-07 20:43:17 -0500

    use remote urls for ad iframe src

--- app/toura/components/AdTag.js
+++ app/toura/components/AdTag.js
@@ -6,13 +6,7 @@ dojo.declare('toura.components.AdTag', toura._Component, {
   templateString : dojo.cache('toura.components', 'AdTag/AdTag.haml'),
 
   adjustMarkup : function() {
-    var src = dojo.string.substitute("http://${host}/${os}/${device}/media/html/ad-${device}.html",
-      {
-        host   : window.location.host,
-        os     : this.device.os,
-        device : this.device.type
-      });
-
+    var src = TouraAppConfig.ads[this.device.type];
     dojo.attr(this.adFrame,"src",src);
   }
 });
