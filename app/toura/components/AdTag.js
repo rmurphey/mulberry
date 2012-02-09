1168e645f6f23406f5d0f32d9cf94e8b452313df
Matt Henry <greenideas@gmail.com>
2012-02-09 15:42:19 -0500

    mess with the DOM in startup, when the component is actually in the DOM

--- app/toura/components/AdTag.js
+++ app/toura/components/AdTag.js
@@ -5,8 +5,17 @@ dojo.require('toura._Component');
 dojo.declare('toura.components.AdTag', toura._Component, {
   templateString : dojo.cache('toura.components', 'AdTag/AdTag.haml'),
 
-  adjustMarkup : function() {
-    var src = toura.app.Config.get("app").ads[this.device.type];
-    dojo.attr(this.adFrame,"src",src);
+  prep : function() {
+  },
+
+  startup : function() {
+    var adConfig = toura.app.Config.get("app").ads,
+        src = adConfig[this.device.type];
+
+    if (src) {
+      dojo.attr(this.adFrame,"src",src);
+    } else {
+      this.destroy();
+    }
   }
 });
