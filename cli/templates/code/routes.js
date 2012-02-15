30cdb087f2d930b3031a153f81fcc3ee1bb70c02
Rebecca Murphey <rmurphey@gmail.com>
2012-02-14 22:52:32 -0500

    remove instructions for command line route creation in empty app

--- cli/templates/code/routes.js
+++ cli/templates/code/routes.js
@@ -1,15 +1,5 @@
 dojo.provide('client.routes');
 
-/**
- * You can define custom routes here. To add a new route, run:
- *
- *    mulberry create route <path matcher>
- *
- * For example:
- *
- *    mulberry create route '/foo/:bar'
- *
- */
 mulberry.routes([
   {
     route : '/home',
