910e47245b7ae20be9d94b152df83f97c45f434a
Rebecca Murphey <rmurphey@gmail.com>
2012-03-05 11:14:34 -0500

    fix feed item list failure
    it seems that since all of our i18n stuff is happening in the mulberry
    namespace, it's important that this require happen inside a file that's
    in them mulberry namespace in order for the required NLS files to end up
    in the build. this is a non-optimal solution, but a better one will require
    dealing with separating the Toura and Mulberry NLS stuff.

--- app/mulberry/app/_base.js
+++ app/mulberry/app/_base.js
@@ -10,3 +10,4 @@ dojo.require('mulberry.app.Notifications');
 dojo.require('mulberry.app.Has');
 dojo.require('mulberry.app._Debug');
 dojo.require('mulberry.components.Debug');
+dojo.require('dojo.date.locale');
