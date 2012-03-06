1b89908aea5047c137bcee7d5042e8091af0a8b0
Matt Henry <greenideas@gmail.com>
2012-03-06 16:22:52 -0500

    Test video behavior in its own spec--not in an integration spec

--- spec/integration/kitchensink/empty_asset_scroller_spec.rb
+++ spec/integration/kitchensink/empty_asset_scroller_spec.rb
@@ -3,13 +3,6 @@ require 'integration/kitchensink/spec_helper'
 include CapybaraSpecHelper
 
 describe "Empty Pages", :type => :request do
-  it "a video page with no videos should not have a player" do
-    DEVICES.each do |d|
-      visit "/#{d[:os]}/#{d[:type]}/#/node/node-empty_videos"
-      page.should_not have_css "video"
-    end
-  end
-
   it "an audios page with no audios should fully render" do
     DEVICES.each do |d|
       visit "/#{d[:os]}/#{d[:type]}/#/node/node-empty_audio_with_images"
