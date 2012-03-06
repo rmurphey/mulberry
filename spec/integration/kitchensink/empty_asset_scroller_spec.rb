d2cf8680de0ba3c2270d1f84ce98ccfc7134b426
Matt Henry <greenideas@gmail.com>
2012-03-06 11:40:39 -0500

    for empty video page, just test that the player doesn't exist

--- spec/integration/kitchensink/empty_asset_scroller_spec.rb
+++ spec/integration/kitchensink/empty_asset_scroller_spec.rb
@@ -3,10 +3,10 @@ require 'integration/kitchensink/spec_helper'
 include CapybaraSpecHelper
 
 describe "Empty Pages", :type => :request do
-  it "should be able to navigate to a videos page with no videos" do
+  it "a video page with no videos should not have a player" do
     DEVICES.each do |d|
       visit "/#{d[:os]}/#{d[:type]}/#/node/node-empty_videos"
-      page.should have_content "page body"
+      page.should_not have_css "video"
     end
   end
 
