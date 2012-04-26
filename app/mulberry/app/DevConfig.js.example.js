cc5c475a4a57615941e4deccfa496e18c8e75bdd
Matt Henry <greenideas@gmail.com>
2012-04-26 14:43:52 -0400

    add override for feed proxy URL

--- app/mulberry/app/DevConfig.js.example.js
+++ app/mulberry/app/DevConfig.js.example.js
@@ -14,3 +14,4 @@ toura.features.fontSize = true; // Allows users to change font size
 toura.features.ads = false; // Allows users to enable ads
 
 mulberry.features.debugToolbar = true; // The makes resetting the DB and using weinre easier
+mulberry.feedProxyUrl = 'http://api.toura.com'; // Point at an alternative feed proxy server
